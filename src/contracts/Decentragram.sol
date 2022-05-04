714af7be4262c51d1db8c89c639aa417a7e2aeeb
Zubair Ismail <zubairismail@Zubairs-MBP.attlocal.net>
2022-05-04 16:22:13 -0400

    Finished version of Decentragram Tutorial

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,91 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    // Store Posts
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Create Posts
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        // Ensures image hash exists
+        require(bytes(_imgHash).length > 0);
+
+        // Ensures post description exists
+        require(bytes(_description).length > 0);
+
+        // Ensures uploader address exists
+        require(msg.sender != address(0x0));
+
+        // Increment image id
+        imageCount++;
+
+        // Add image to contract
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+
+        // Trigger an event
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // Tip Posts
+    function tipImageOwner(uint256 _id) public payable {
+        // Id is valid
+        require(_id > 0 && _id <= imageCount);
+        // Get post
+        Image memory _image = images[_id];
+
+        // Get author
+        address payable _author = _image.author;
+
+        // Tip author with desired amount
+        address(_author).transfer(msg.value);
+
+        // Increment tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+
+        // Update the image
+        images[_id] = _image;
+
+        // Trigger an event
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
--- test/test.js
+++ test/test.js
@@ -28,7 +28,8 @@ contract('Decentragram', ([deployer, author, tipper]) => {
 
   describe('images', async () => {
     let result, imageCount
-    const hash = 'QmV8cfu6n4NT5xRr2AHdKxFMTZEJrA44qgrBCr739BN9Wb'
+    //const hash = 'QmV8cfu6n4NT5xRr2AHdKxFMTZEJrA44qgrBCr739BN9Wb'
+    const hash = 'abc123'
 
     before(async () => {
       result = await decentragram.uploadImage(hash, 'Image description', { from: author })
@@ -47,6 +48,7 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(event.author, author, 'author is correct')
 
 
+
       // FAILURE: Image must have hash
       await decentragram.uploadImage('', 'Image description', { from: author }).should.be.rejected;
 
@@ -62,39 +64,40 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(image.description, 'Image description', 'description is correct')
       assert.equal(image.tipAmount, '0', 'tip amount is correct')
       assert.equal(image.author, author, 'author is correct')
-    })
 
-    it('allows users to tip images', async () => {
-      // Track the author balance before purchase
-      let oldAuthorBalance
-      oldAuthorBalance = await web3.eth.getBalance(author)
-      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+      it('allows users to tip images', async () => {
+        // Track the author balance before purchase
+        let oldAuthorBalance
+        oldAuthorBalance = await web3.eth.getBalance(author)
+        oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
 
-      result = await decentragram.tipImageOwner(imageCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+        result = await decentragram.tipImageOwner(imageCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
 
-      // SUCCESS
-      const event = result.logs[0].args
-      assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
-      assert.equal(event.hash, hash, 'Hash is correct')
-      assert.equal(event.description, 'Image description', 'description is correct')
-      assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
-      assert.equal(event.author, author, 'author is correct')
+        // SUCCESS
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
+        assert.equal(event.hash, hash, 'Hash is correct')
+        assert.equal(event.description, 'Image description', 'description is correct')
+        assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
+        assert.equal(event.author, author, 'author is correct')
 
-      // Check that author received funds
-      let newAuthorBalance
-      newAuthorBalance = await web3.eth.getBalance(author)
-      newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+        // Check that author received funds
+        let newAuthorBalance
+        newAuthorBalance = await web3.eth.getBalance(author)
+        newAuthorBalance = new web3.utils.BN(newAuthorBalance)
 
-      let tipImageOwner
-      tipImageOwner = web3.utils.toWei('1', 'Ether')
-      tipImageOwner = new web3.utils.BN(tipImageOwner)
+        let tipImageOwner
+        tipImageOwner = web3.utils.toWei('1', 'Ether')
+        tipImageOwner = new web3.utils.BN(tipImageOwner)
 
-      const expectedBalance = oldAuthorBalance.add(tipImageOwner)
+        const expectedBalance = oldAuthorBalance.add(tipImageOwner)
 
-      assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+        assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
 
-      // FAILURE: Tries to tip a image that does not exist
-      await decentragram.tipImageOwner(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+        // FAILURE: Tries to tip a image that does not exist
+        await decentragram.tipImageOwner(99, { from: tipper, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+
+      })
     })
   })
 })
